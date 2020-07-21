fa4e7ba127255ffbbde912109c492f29dad24cd5
mjlane <mjlane@monsanto.com>
2020-07-21 17:23:16 -0500

    now have a hand made router

--- index.js
+++ index.js
@@ -3,22 +3,43 @@ const express = require("express")
 
 const app = express()
 
+const homeResponse = (req,res) => {
+  res.json({
+    hi: "there",
+    whats: "up",
+  })
+}
+
+const bestTvShow = (req, res) => {
+  res.send("Parks and Rec")
+}
+
+const absurdTvShow = (req, res) => {
+  res.send("The Order")
+}
+
+const fourOhFour = (req, res) => {
+  res.status(404)
+  res.send("Four Oh Four :( ")
+}
+
+const routes = {
+  'GET /': homeResponse,
+  'GET /bestTv': bestTvShow,
+  'GET /absurdTv': absurdTvShow
+}
+
+
+
 app.use((req, res) => {
-
-  const method = req.method
-  const route = req.url
-
-  if (route === "/" && method === "GET") {
-    const someObject = {
-      hi: "there",
-      whats: "up",
-    }
-
-    res.send(someObject)
+  const routeKey = req.method+ ' ' + req.url
+  const desiredRouteFunction = routes[routeKey]
+  
+  if (desiredRouteFunction !== undefined) {
+    desiredRouteFunction(req, res)
+  } else {
+    fourOhFour(req, res)
   }
-
-
-  res.end("The method request was " + method + " and the route was " + route)
 })
 
 const port = 3535

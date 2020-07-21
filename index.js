bd9c1719b858d473da70eca1400e335b6267f2c9
mjlane <mjlane@monsanto.com>
2020-07-21 16:52:32 -0500

    added express

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const http = require("http")
+const express = require("express")
 
+const app = express()
 
-const server = http.createServer( (req, res) => { 
+app.use((req, res) => {
 
   const method = req.method
   const route = req.url
+
+  if (route === "/" && method === "GET") {
+    const someObject = {
+      hi: "there",
+      whats: "up",
+    }
+
+    res.send(someObject)
+  }
 
 
   res.end("The method request was " + method + " and the route was " + route)
 })
 
 const port = 3535
-server.listen(port, console.log(`Now listening on port ${port}: 🚢⛴🛳`))
+app.listen(port, console.log(`Now listening on port ${port}: 🚢⛴🛳`))
 

66e03534b0872e9e6b0607d79f4f9885b8378f67
mjlane <mjlane@monsanto.com>
2020-07-30 18:02:49 -0500

    updated code for getting user by username

--- userRoute.js
+++ userRoute.js
@@ -20,12 +20,28 @@ const addUserToDatabase = async (req, res) => {
     res.json(results)
 }
 
+
+const findUserByUserName = async (req, res) => {
+  const userName = req.body.userName
+
+
+  const results = await userModel.find({
+    userName
+  })
+
+  console.log(results)
+  res.json(results)
+
+}
+
+
 const userRouter = express.Router()
 
 
 
 
 userRouter.route('/add').post( bodyParser.json(), addUserToDatabase) 
+userRouter.route('/find').post(bodyParser.json(), findUserByUserName)
 
 module.exports = userRouter
 
